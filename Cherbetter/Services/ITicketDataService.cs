1358f7eab2316f71491c9b309924e89a7758da49
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-05-12 15:50:44 -0400

    Added the Update and Delete function to Ticket DAL

--- Cherbetter/Services/ITicketDataService.cs
+++ Cherbetter/Services/ITicketDataService.cs
@@ -9,7 +9,7 @@ namespace Cherbetter.Services
     public interface ITicketDataService
     {
         public IEnumerable<Ticket> GetTicketsAll();
-        public int AddTicket(Ticket Ticket);
+        public int AddTicket(Ticket ticket);
         public int DeleteTicketByID(int id);
         public Ticket GetTicketByID(int id);
         public int UpdateTicket(Ticket ticket);
