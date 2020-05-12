a350d30fd88977bc990ff54bd0506117ad2ca92d
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-05-12 10:44:58 -0400

    added models

---
+++ Cherbetter/Models/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cherbetter.Models
+{
+    public class Favorite
+    {
+        public int FID { get; set; }
+        public string Email { get; set; }
+        public int TicketID { get; set; }
+
+    }
+}
---
+++ Cherbetter/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cherbetter.Models
+{
+    public class Ticket
+    {
+        public int TID { get; set; }
+        public string Title { get; set; }
+        public string TDescription { get; set; }
+        public int TStatus { get; set; }
+        public string Resolution { get; set; }
+    }
+}
