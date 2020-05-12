6041e6184bea334ef589e3ef6e35679527eb44a7
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-05-12 11:11:06 -0400

    Updated simple CRUD interface for DAL

--- Cherbetter/Services/ITicketDataService.cs
+++ Cherbetter/Services/ITicketDataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cherbetter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace Cherbetter.Services
 {
     public interface ITicketDataService
     {
+        public IEnumerable<Ticket> GetTicketsAll();
+        public int AddTicket(Ticket Ticket);
+        public int DeleteTicketByID(int id);
+        public Ticket GetTicketByID(int id);
+        public int UpdateTicket(Ticket ticket);
     }
 }
